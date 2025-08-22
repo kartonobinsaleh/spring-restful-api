57a94396cfd98e6a953fcf7105cf39749f37ea01
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-22 22:03:31 +0700

    feat(entity): add Address entity with JPA mapping

---
+++ src/main/java/com/myapp/belajar_spring_restful_api/entity/Address.java
@@ -0,0 +1,39 @@
+package com.myapp.belajar_spring_restful_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    private String id;
+
+    private String street;
+
+    private String city;
+
+
+    private String province;
+
+    private String country;
+
+    @Column(name = "post_code")
+    private String postCode;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+    
+}
--- src/main/java/com/myapp/belajar_spring_restful_api/entity/Contact.java
+++ src/main/java/com/myapp/belajar_spring_restful_api/entity/Contact.java
@@ -1,10 +1,13 @@
 package com.myapp.belajar_spring_restful_api.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,4 +37,7 @@ public class Contact {
     @ManyToOne
     @JoinColumn(name = "username", referencedColumnName = "username")
     private User user;
+
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addresses;
 }
