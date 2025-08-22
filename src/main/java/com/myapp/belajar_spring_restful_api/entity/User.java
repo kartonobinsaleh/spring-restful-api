3245af216c6ad7b3e1d56e14a360e98b86dc52da
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-22 21:45:07 +0700

    feat(entity): add User entity with JPA mapping

---
+++ src/main/java/com/myapp/belajar_spring_restful_api/entity/User.java
@@ -0,0 +1,29 @@
+package com.myapp.belajar_spring_restful_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+
+public class User {
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+}
