8360f4f465ac575dbc81ab0dbce644d5cf6b85b1
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-23 10:16:10 +0700

    fix(entity): add no-args constructor to User entity

--- src/main/java/com/myapp/belajar_spring_restful_api/entity/User.java
+++ src/main/java/com/myapp/belajar_spring_restful_api/entity/User.java
@@ -9,11 +9,13 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="users")
 
