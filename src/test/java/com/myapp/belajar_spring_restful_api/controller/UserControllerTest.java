8d0db901d10206f8aa5ec2e35c3591d2b4080f24
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-23 17:54:28 +0700

    feat(test): Add UserControllerTest to verify user registration endpoint

---
+++ src/test/java/com/myapp/belajar_spring_restful_api/controller/UserControllerTest.java
@@ -0,0 +1,63 @@
+package com.myapp.belajar_spring_restful_api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myapp.belajar_spring_restful_api.model.ApiResponse;
+import com.myapp.belajar_spring_restful_api.model.RegisterUserRequest;
+import com.myapp.belajar_spring_restful_api.repository.UserRepository;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void testRegisterSuccess() throws Exception {
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("test");
+        request.setPassword("rahasia");
+        request.setName("Test");
+
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isOk())
+                .andDo(result -> {
+                    ApiResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                    assertEquals("OK", response.getData());
+                });
+    }
+}
