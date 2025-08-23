76e2906a141126d317132ffe89017aae62381bbe
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-23 11:08:59 +0700

    feat(service): implement UserService with register user logic and validation

---
+++ src/main/java/com/myapp/belajar_spring_restful_api/service/UserService.java
@@ -0,0 +1,46 @@
+package com.myapp.belajar_spring_restful_api.service;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myapp.belajar_spring_restful_api.entity.User;
+import com.myapp.belajar_spring_restful_api.exception.ApiException;
+import com.myapp.belajar_spring_restful_api.model.RegisterUserRequest;
+import com.myapp.belajar_spring_restful_api.repository.UserRepository;
+import com.myapp.belajar_spring_restful_api.security.BCrypt;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Validator validator;
+
+    @Transactional
+    public void register(RegisterUserRequest request){
+        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(request);
+        if(constraintViolations.size() != 0){
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        if(userRepository.existsById(request.getUsername())){
+            throw new ApiException("Username already registered");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        user.setName(request.getName());
+
+        userRepository.save(user);
+
+    }
+}
