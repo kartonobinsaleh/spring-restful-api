2fae0efdfecb330abe4e1ad2f7bbadbde0d32a33
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-24 10:52:22 +0700

    test(auth): add login success test with token and expiration validation

--- src/test/java/com/myapp/belajar_spring_restful_api/controller/AuthControllerTest.java
+++ src/test/java/com/myapp/belajar_spring_restful_api/controller/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package com.myapp.belajar_spring_restful_api.controller;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myapp.belajar_spring_restful_api.entity.User;
 import com.myapp.belajar_spring_restful_api.model.ApiResponse;
 import com.myapp.belajar_spring_restful_api.model.LoginUserRequest;
+import com.myapp.belajar_spring_restful_api.model.TokenResponse;
 import com.myapp.belajar_spring_restful_api.repository.UserRepository;
 import com.myapp.belajar_spring_restful_api.security.BCrypt;
 
@@ -85,6 +88,42 @@ class AuthControllerTest {
                             });
 
                     assertNotNull(response.getErrors());
+                });
+    }
+
+    @Test
+    void loginSuccess() throws Exception {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("rahasia", BCrypt.gensalt()));
+        user.setName("Test");
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("rahasia");
+
+        mockMvc.perform(
+                post("/api/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isOk())
+                .andDo(result -> {
+                    ApiResponse<TokenResponse> response = objectMapper.readValue(
+                            result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+
+                            });
+
+                    assertNull(response.getErrors());
+
+                    assertNotNull(response.getData().getToken());
+                    assertNotNull(response.getData().getExpiredAt());
+
+                    User userDb = userRepository.findById("test").orElse(null);
+                    assertNotNull(userDb);
+                    assertEquals(userDb.getToken(), response.getData().getToken());
+                    assertEquals(userDb.getTokenExpiredAt(), response.getData().getExpiredAt());
                 });
     }
 }
