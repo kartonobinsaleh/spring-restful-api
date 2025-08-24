24b517438776745e68616c6fd39b7152af691007
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-24 10:25:26 +0700

    feat(auth): add AuthController with login endpoint

---
+++ src/main/java/com/myapp/belajar_spring_restful_api/controller/AuthContoller.java
@@ -0,0 +1,25 @@
+package com.myapp.belajar_spring_restful_api.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myapp.belajar_spring_restful_api.model.ApiResponse;
+import com.myapp.belajar_spring_restful_api.model.LoginUserRequest;
+import com.myapp.belajar_spring_restful_api.model.TokenResponse;
+import com.myapp.belajar_spring_restful_api.service.AuthService;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class AuthContoller {
+
+    private AuthService authService;
+
+    @PostMapping(path = "/api/auth/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
+        TokenResponse tokenResponse = authService.login(request);
+
+        return ApiResponse.<TokenResponse>builder().data(tokenResponse).build();
+    }
+}
