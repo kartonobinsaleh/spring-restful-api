84049526061d6967e64271dfea70972cb49e1339
kartonobinsaleh <kartono.saleh@gmail.com>
2025-08-24 10:44:50 +0700

    test(auth): add integration test for login failure scenario password wrong

--- src/test/java/com/myapp/belajar_spring_restful_api/controller/AuthControllerTest.java
+++ src/test/java/com/myapp/belajar_spring_restful_api/controller/AuthControllerTest.java
@@ -15,9 +15,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myapp.belajar_spring_restful_api.entity.User;
 import com.myapp.belajar_spring_restful_api.model.ApiResponse;
 import com.myapp.belajar_spring_restful_api.model.LoginUserRequest;
 import com.myapp.belajar_spring_restful_api.repository.UserRepository;
+import com.myapp.belajar_spring_restful_api.security.BCrypt;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -43,6 +45,33 @@ class AuthControllerTest {
         LoginUserRequest request = new LoginUserRequest();
         request.setUsername("test");
         request.setPassword("test");
+
+        mockMvc.perform(
+                post("/api/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isUnauthorized())
+                .andDo(result -> {
+                    ApiResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+
+                            });
+
+                    assertNotNull(response.getErrors());
+                });
+    }
+
+    @Test
+    void loginFailedWrongPassword() throws Exception {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("rahasia", BCrypt.gensalt()));
+        user.setName("Test");
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("salah");
 
         mockMvc.perform(
                 post("/api/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
